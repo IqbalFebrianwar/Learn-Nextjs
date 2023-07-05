af6500033d6b4324a97c7091970cccb400d65377
IqbalFebrianwar <zonelyrics26@gmail.com>
2023-07-06 00:48:48 +0700

    Benerin Input Page

--- src/app/components/Elements/Input/Input.jsx
+++ src/app/components/Elements/Input/Input.jsx
@@ -4,7 +4,7 @@ const Input =(props)=>{
     return(
         <input 
             type={type}
-            className="shadow-lg px-3 py-2 mb-5 text-sm text-black border rounded 2-full placeholder=opacity-50"
+            className="px-3 py-2 mb-5 text-sm text-black rounded 2-full placeholder=opacity-50"
             placeholder= {placeholder}
             name={name}
         />
