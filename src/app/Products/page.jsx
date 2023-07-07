9473d9942725578caff5511cf7cd5e3b678ce62f
IqbalFebrianwar <zonelyrics26@gmail.com>
2023-07-07 09:50:37 +0700

    Products

--- src/app/Products/page.jsx
+++ src/app/Products/page.jsx
@@ -7,15 +7,15 @@ const ProductPage =()=> {
             <ProductCard>
                 <ProductCard.ImageCard image="https://img.freepik.com/free-photo/flat-lay-arrangement-with-running-shoes-towel_23-2148259118.jpg?w=900&t=st=1688695167~exp=1688695767~hmac=5725571f3a23b6a0b7f78e75a645db3b00bc1bdbdc8f4e7167fc92208fcbcd70"></ProductCard.ImageCard>
                 <ProductCard.MainCard>
-                <ProductCard.ChildrenCard title="Sepatu Nike" children="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iste exercitationem molestiae modi aliquid qui, fugit, consequuntur reprehenderit ea quas placeat sunt libero dolore blanditiis quia voluptates consectetur hic? Cum?"/>
+                <ProductCard.ChildrenCard title="Nike" children="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iste exercitationem molestiae modi aliquid qui, fugit, consequuntur reprehenderit ea quas placeat sunt libero dolore blanditiis quia voluptates consectetur hic? Cum?"/>
                 <ProductCard.ButtonCard children="Beli" />
                 </ProductCard.MainCard>
             </ProductCard>
 
             <ProductCard>
-                <ProductCard.ImageCard image="https://img.freepik.com/free-photo/flat-lay-arrangement-with-running-shoes-towel_23-2148259118.jpg?w=900&t=st=1688695167~exp=1688695767~hmac=5725571f3a23b6a0b7f78e75a645db3b00bc1bdbdc8f4e7167fc92208fcbcd70"></ProductCard.ImageCard>
+                <ProductCard.ImageCard image="https://img.freepik.com/free-photo/close-up-legs-blue-sneakers-riding-green-skateboard-motion-active-urban-lifestyle-youth-training-hobby-activity-active-outdoor-sport-kids-child-skateboarding_158595-6161.jpg?w=900&t=st=1688698033~exp=1688698633~hmac=db78d0a83876bd92eca917a4c44867be77a73f1794e8f637ac67c2d364e86b27"></ProductCard.ImageCard>
                 <ProductCard.MainCard>
-                <ProductCard.ChildrenCard title="Sepatu Nike" children="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iste exercitationem molestiae modi aliquid qui, fugit, consequuntur reprehenderit ea quas placeat sunt libero dolore blanditiis quia voluptates consectetur hic? Cum?"/>
+                <ProductCard.ChildrenCard title="Adidas" children="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iste exercitationem molestiae modi aliquid qui, fugit, consequuntur reprehenderit ea quas placeat sunt libero dolore blanditiis quia voluptates consectetur hic? Cum?"/>
                 <ProductCard.ButtonCard children="Beli" />
                 </ProductCard.MainCard>
             </ProductCard>
