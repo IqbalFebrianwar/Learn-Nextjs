3d05a287ea2ddfac44bb7d0af10356396da4b2fd
IqbalFebrianwar <zonelyrics26@gmail.com>
2023-07-07 11:03:50 +0700

    Learn Rendering Lists

--- src/app/Products/page.jsx
+++ src/app/Products/page.jsx
@@ -1,24 +1,42 @@
 import ProductCard from "../components/Fragments/ProductCard";
 
+const products =[
+    {
+        id:1,
+        title:"Nike",
+        image:"https://img.freepik.com/free-photo/flat-lay-arrangement-with-running-shoes-towel_23-2148259118.jpg?w=900&t=st=1688695167~exp=1688695767~hmac=5725571f3a23b6a0b7f78e75a645db3b00bc1bdbdc8f4e7167fc92208fcbcd70",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iste exercitationem molestiae modi aliquid qui, fugit, consequuntur reprehenderit ea quas placeat sunt libero dolore blanditiis quia voluptates consectetur hic? Cum?'
+    
+    },
+    {
+        id:2,
+        title:"Adidas",
+        image:"https://img.freepik.com/free-photo/flat-lay-arrangement-with-running-shoes-towel_23-2148259118.jpg?w=900&t=st=1688695167~exp=1688695767~hmac=5725571f3a23b6a0b7f78e75a645db3b00bc1bdbdc8f4e7167fc92208fcbcd70",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iste exercitationem molestiae modi aliquid qui, fugit, consequuntur reprehenderit ea quas placeat sunt libero dolore blanditiis quia voluptates consectetur hic? Cum?'
+    
+    },
+    {
+        id:3,
+        title:"Puma",
+        image:"https://img.freepik.com/free-photo/flat-lay-arrangement-with-running-shoes-towel_23-2148259118.jpg?w=900&t=st=1688695167~exp=1688695767~hmac=5725571f3a23b6a0b7f78e75a645db3b00bc1bdbdc8f4e7167fc92208fcbcd70",
+        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iste exercitationem molestiae modi aliquid qui, fugit, consequuntur reprehenderit ea quas placeat sunt libero dolore blanditiis quia voluptates consectetur hic? Cum?'
+    
+    }
+]
+
 const ProductPage =()=> {
     return(
         <>
         <div className="md:items-center md:justify-center md:flex justify-items-center">
-            <ProductCard>
-                <ProductCard.ImageCard image="https://img.freepik.com/free-photo/flat-lay-arrangement-with-running-shoes-towel_23-2148259118.jpg?w=900&t=st=1688695167~exp=1688695767~hmac=5725571f3a23b6a0b7f78e75a645db3b00bc1bdbdc8f4e7167fc92208fcbcd70"></ProductCard.ImageCard>
+            {products.map ( (product)=>(
+                <ProductCard>
+                <ProductCard.ImageCard image={product.image} />
                 <ProductCard.MainCard>
-                <ProductCard.ChildrenCard title="Nike" children="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iste exercitationem molestiae modi aliquid qui, fugit, consequuntur reprehenderit ea quas placeat sunt libero dolore blanditiis quia voluptates consectetur hic? Cum?"/>
-                <ProductCard.ButtonCard children="Beli" />
+                <ProductCard.ChildrenCard title={product.title} children={product.description}/>
+                <ProductCard.ButtonCard />
                 </ProductCard.MainCard>
-            </ProductCard>
-
-            <ProductCard>
-                <ProductCard.ImageCard image="https://img.freepik.com/free-photo/close-up-legs-blue-sneakers-riding-green-skateboard-motion-active-urban-lifestyle-youth-training-hobby-activity-active-outdoor-sport-kids-child-skateboarding_158595-6161.jpg?w=900&t=st=1688698033~exp=1688698633~hmac=db78d0a83876bd92eca917a4c44867be77a73f1794e8f637ac67c2d364e86b27"></ProductCard.ImageCard>
-                <ProductCard.MainCard>
-                <ProductCard.ChildrenCard title="Adidas" children="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iste exercitationem molestiae modi aliquid qui, fugit, consequuntur reprehenderit ea quas placeat sunt libero dolore blanditiis quia voluptates consectetur hic? Cum?"/>
-                <ProductCard.ButtonCard children="Beli" />
-                </ProductCard.MainCard>
-            </ProductCard>
+                </ProductCard>
+            ))}
         </div>
         </>
     )
--- src/app/components/Fragments/ProductCard.jsx
+++ src/app/components/Fragments/ProductCard.jsx
@@ -1,7 +1,7 @@
 const ProductCard =(props)=> {
     const {children} = props;
     return (
-            <div className="w-full max-w-xs mx-3 my-10 bg-white border rounded-lg shadow">
+            <div className="flex flex-col justify-between w-full max-w-xs mx-3 my-10 bg-white border rounded-lg shadow">
                 {children}
             </div>
     );
@@ -10,7 +10,7 @@ const ProductCard =(props)=> {
 const MainCard =(props)=> {
     const {children} = props;
     return (
-        <div class="p-5">
+        <div class="p-5 mb-2">
             {children}
         </div>
     );
@@ -19,7 +19,7 @@ const MainCard =(props)=> {
 const ImageCard =(props)=> {
     const {image} = props;
     return (
-        <img class="rounded-t-lg py-5 px-5" src={image} alt="" />
+        <img className="px-5 py-5 rounded-t-lg" src={image} alt="" />
     );
 };
 
@@ -27,18 +27,17 @@ const ChildrenCard =(props)=> {
     const {title, children} = props;
     return (
         <>
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">{title}</h5>
-            <p class="mb-3 font-normal text-black">{children}</p>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-black">{title}</h5>
+            <p className="mb-3 font-normal text-black">{children}</p>
         </>
     );
 };
 
-const ButtonCard =(props)=> {
-    const {children} = props;
+const ButtonCard =()=> {
     return (
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            {children}
-            <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+        <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Beli
+            <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
         </a>
