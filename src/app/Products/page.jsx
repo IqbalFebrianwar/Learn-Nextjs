adda44d9c839fcf54c6212b8c1a27e4aa3ac67dc
IqbalFebrianwar <zonelyrics26@gmail.com>
2023-09-21 14:43:10 +0700

    test

--- src/app/Products/page.jsx
+++ src/app/Products/page.jsx
@@ -5,7 +5,7 @@ const products =[
         id:1,
         title:"Nike",
         image:"https://img.freepik.com/free-photo/flat-lay-arrangement-with-running-shoes-towel_23-2148259118.jpg?w=900&t=st=1688695167~exp=1688695767~hmac=5725571f3a23b6a0b7f78e75a645db3b00bc1bdbdc8f4e7167fc92208fcbcd70",
-        description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel iste exercitationem molestiae modi aliquid qui, fugit, consequuntur reprehenderit ea quas placeat sunt libero dolore blanditiis quia voluptates consectetur hic? Cum?'
+        description:'Lorem ipsum pppdolor sit amet consectetur adipisicing elit. Vel iste exercitationem molestiae modi aliquid qui, fugit, consequuntur reprehenderit ea quas placeat sunt libero dolore blanditiis quia voluptates consectetur hic? Cum?'
     
     },
     {
